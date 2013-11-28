376474489e2d9a2ef838c6f559adaef18903bd97
Andy Bryson <agbryson@gmail.com>
2013-11-28 12:59:46 +0000

    Remove binaries from git

--- WsLoggerLinux/install.sh
+++
@@ -1,5 +0,0 @@
-cp -r WebSocketLogger /usr/lib/
-echo '#!/bin/bash' > WebSocketLogger.sh
-echo 'mono /usr/lib/WebSocketLogger/WsLogger.exe' >> WebSocketLogger.sh
-mv WebSocketLogger.sh /usr/bin/WebSocketLogger
-chmod 755 /usr/bin/WebSocketLogger
