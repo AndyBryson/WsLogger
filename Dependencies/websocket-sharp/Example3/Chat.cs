9a8c4aff811b82d7d074d112529b683cff1002db
Andy Bryson <agbryson@gmail.com>
2013-11-28 23:23:24 +0000

    sorting dependencies

---
+++ Dependencies/websocket-sharp/Example3/Chat.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example3
+{
+  public class Chat : WebSocketService
+  {
+    private static int _num = 0;
+
+    private string _name;
+    private string _prefix;
+
+    public Chat ()
+      : this ("anon#")
+    {
+    }
+
+    public Chat (string prefix)
+    {
+      _prefix = prefix;
+    }
+
+    private string getName ()
+    {
+      return Context.QueryString ["name"] ?? (_prefix + getNum ());
+    }
+
+    private int getNum ()
+    {
+      return Interlocked.Increment (ref _num);
+    }
+
+    protected override void OnOpen ()
+    {
+      _name = getName ();
+    }
+
+    protected override void OnMessage (MessageEventArgs e)
+    {
+      Sessions.Broadcast (String.Format ("{0}: {1}", _name, e.Data));
+    }
+
+    protected override void OnClose (CloseEventArgs e)
+    {
+      Sessions.Broadcast (String.Format ("{0} got logged off...", _name));
+    }
+  }
+}
---
+++ Dependencies/websocket-sharp/Example3/Program.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+
+namespace Example3
+{
+  public class Program
+  {
+    private static HttpServer _httpsv;
+
+    public static void Main (string [] args)
+    {
+      _httpsv = new HttpServer (4649);
+      //_httpsv = new HttpServer (4649, true);
+      #if DEBUG
+      _httpsv.Log.Level = LogLevel.TRACE;
+      #endif
+      _httpsv.RootPath = ConfigurationManager.AppSettings ["RootPath"];
+      //var cert = ConfigurationManager.AppSettings ["ServerCertFile"];
+      //var password = ConfigurationManager.AppSettings ["CertFilePassword"];
+      //_httpsv.Certificate = new X509Certificate2 (cert, password);
+      //_httpsv.KeepClean = false;
+      _httpsv.AddWebSocketService<Echo> ("/Echo");
+      _httpsv.AddWebSocketService<Chat> ("/Chat");
+      //_httpsv.AddWebSocketService<Chat> ("/Chat", () => new Chat ("Anon#"));
+
+      _httpsv.OnGet += (sender, e) =>
+      {
+        onGet (e);
+      };
+
+      _httpsv.Start ();
+      if (_httpsv.IsListening)
+      {
+        Console.WriteLine (
+          "An HTTP Server listening on port: {0} WebSocket service paths:", _httpsv.Port);
+
+        foreach (var path in _httpsv.WebSocketServices.ServicePaths)
+          Console.WriteLine ("  {0}", path);
+      }
+
+      Console.WriteLine ("\nPress Enter key to stop the server...");
+      Console.ReadLine ();
+
+      _httpsv.Stop ();       
+    }
+
+    private static byte [] getContent (string path)
+    {
+      if (path == "/")
+        path += "index.html";
+
+      return _httpsv.GetFile (path);
+    }
+
+    private static void onGet (HttpRequestEventArgs eventArgs)
+    {
+      var request = eventArgs.Request;
+      var response = eventArgs.Response;
+      var content = getContent (request.RawUrl);
+      if (content != null)
+      {
+        response.WriteContent (content);
+        return;
+      }
+
+      response.StatusCode = (int) HttpStatusCode.NotFound;
+    }
+  }
+}
